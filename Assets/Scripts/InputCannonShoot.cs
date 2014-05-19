957d86167424fe35cb35b8d4d0b0cc7e8a31cd7e
SenorConeho <senorconeho@gmail.com>
2014-05-18 23:04:30 -0300

    Cannon shooting and doing damage on fighters

---
+++ Assets/Scripts/InputCannonShoot.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Class description here
+/// </summary>
+public class InputCannonShoot : MonoBehaviour {
+	
+	public CWeapon	weaponScript = null;
+
+
+	/* -----------------------------------------------------------------------------------------------------------
+	 * UNITY'S STUFF HERE
+	 * -----------------------------------------------------------------------------------------------------------
+	 */
+	/// <summary>
+	/// Will execute when loaded
+	/// </summary>
+	void Awake() {
+
+	}
+
+	/// <summary>
+	/// Start will execute when an object with it is instantiated
+	/// </summary>
+	void Start () {
+	
+		weaponScript = gameObject.GetComponent<CWeapon>();
+		weaponScript.trShooter = this.transform;
+	}
+	
+	/// <summary>
+	/// Update is called once per frame
+	/// </summary>
+	void Update () {
+
+		if(Input.GetMouseButton(0)) {
+
+			weaponScript.Shoot();
+		}
+
+	}
+
+	/* -----------------------------------------------------------------------------------------------------------
+	 * MY STUFF HERE
+	 * -----------------------------------------------------------------------------------------------------------
+	 */
+
+}
---
+++ Assets/Scripts/Weapons/CWeapon.cs
@@ -0,0 +1,133 @@
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Simple weapon fire, based on the Angry Bots weapon code
+/// How does this work?
+/// We need a place to spawn the projectiles - trSpawnPoint
+/// The projectile itself is a prefab - bulletPrefab
+/// </summary>
+public class CWeapon : MonoBehaviour {
+
+	/* ==========================================================================================================
+	 * CLASS VARIABLES
+	 * ==========================================================================================================
+	 */
+	// PUBLIC
+	public GameObject		bulletPrefab			=	null;	//< Bullet to be instantiated when the weapon fires
+	public GameObject		particlePrefab		= null;	//< Particle shown when the weapon shoot
+	public Transform		trSpawnPoint			= null;	//< Where the bullet will be instantiated
+	public float				fFrequency				= 10;		//< shots per second
+	public float				fDamagePerSecond	= 20;		//< Damage inflicted per second. FIXME: this should be in the projectile class		
+	public Transform		trShooter					= null;	//< Who is shooting with this weapon
+	public CBaseActor		actorShooterScript = null;
+
+	// PRIVATE
+	protected float				fSpawnTime		= 0.0f;	//< internal TTL timer
+	protected float				fLastFireTime = -1;		//< Time when the last bullet was fired
+	protected bool				bnIsFiring		= false;//< Are we firing right now?
+
+	/* ==========================================================================================================
+	 * UNITY METHODS
+	 * ==========================================================================================================
+	 */
+	public virtual void Awake() {
+
+		if( trSpawnPoint == null) {
+
+			trSpawnPoint = transform;
+		}
+
+		if(trShooter) {
+
+			actorShooterScript = trShooter.gameObject.GetComponent<CBaseActor>();
+		}
+	}
+
+	/// <summary>
+	/// Shoots the projectile
+	/// </summary>
+	public void Shoot() {
+
+		if(bnIsFiring)
+			return;
+
+		bnIsFiring = true;
+
+		// Spawn a bullet
+		// Using the spawner
+		//GameObject go = Spawner.Spawn(bulletPrefab, trSpawnPoint.position, trSpawnPoint.rotation) as GameObject;
+		GameObject go = Instantiate(bulletPrefab, trSpawnPoint.position, trSpawnPoint.rotation) as GameObject;
+		CBasicProjectile bulletScript = go.GetComponent<CBasicProjectile>();
+
+		fLastFireTime = Time.time;
+
+		bulletScript.fDistance = 1000;
+		bulletScript.trShooter = trShooter;
+	}
+	
+	// Update is called once per frame
+	public virtual void Update () {
+
+		if(bnIsFiring) {
+
+			if(Time.time > fLastFireTime + ( 1/fFrequency )) {
+
+				bnIsFiring = false;
+			}
+		}
+	}
+
+	/// <summary>
+	///
+	/// </summary>
+	public virtual void FixedUpdate() {
+
+	}
+	/* ==========================================================================================================
+	 * CLASS METHODS
+	 * ==========================================================================================================
+	 */
+	/// <summary>
+	/// Firing weapon
+	/// </summary>
+	public virtual void StartFire() {
+
+		bnIsFiring = true;
+
+		if(audio)
+			audio.Play();
+
+		// TODO: add audio source
+	}
+
+	/// <summary>
+	/// Resume firing
+	/// </summary>
+	public virtual void StopFire() {
+
+		bnIsFiring = false;
+		
+		if(audio)
+			audio.Stop();
+	}
+
+	/// <summary>
+	/// Is this weapon firing right now?
+	/// </summary>
+	/// <returns> True if we are firing, false otherwise<returns>
+	public virtual bool IsFiring() {
+
+		return bnIsFiring;
+	}
+
+	void OnEnable() {
+
+		//bnIsFiring = false;
+	}
+
+	void OnDisable() {
+
+		//bnIsFiring = false;
+	}
+}
