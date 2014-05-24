b17ee91c3ef5dc8f3fc3ca1ce3462b44e7f7b2a2
SenorConeho <senorconeho@gmail.com>
2014-05-24 14:13:43 -0300

    Starting to add movement to the cruiser
    Added a script to handle the mouse cursor location
    Added a script to do me cruiser movement
    Added a new cruiser prefab and a new scene

---
+++ Assets/Scripts/Cruiser/InputCruiserMovement.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Class description here
+/// </summary>
+public class InputCruiserMovement : MonoBehaviour {
+	
+	/* -----------------------------------------------------------------------------------------------------------
+	 * UNITY'S STUFF HERE
+	 * -----------------------------------------------------------------------------------------------------------
+	 */
+
+	public Texture2D	txCursorSelectPosition;		//< Cursor to select the ship's position
+	Texture2D txCurrentCursor = null;
+	public Vector3							vGUICursorPosition;
+	public string	stMenuMessage = null;
+
+	int nMenuLevel = 0;
+	InputGenericMouseCursorPosition	mouseScript;
+
+	/// <summary>
+	/// Will execute when loaded
+	/// </summary>
+	void Awake() {
+
+	}
+
+	/// <summary>
+	/// Start will execute when an object with it is instantiated
+	/// </summary>
+	void Start () {
+	
+		mouseScript = GetComponent<InputGenericMouseCursorPosition>();
+	}
+	
+	/// <summary>
+	/// Update is called once per frame
+	/// </summary>
+	void Update () {
+	
+		// Input
+		if(Input.GetKey(KeyCode.M)) {
+
+			// Movement Menu
+			nMenuLevel = 1;
+		}
+
+		if(Input.GetMouseButton(0) && nMenuLevel == 1) {
+			// Confirming the move position
+			nMenuLevel++;
+		}
+
+		if(Input.GetKey(KeyCode.Escape)) {
+
+			// Return to a previous state
+			nMenuLevel -=1;
+			if(nMenuLevel < 0)
+				nMenuLevel = 0;
+		}
+
+		ProcessMovementMenuLevel(nMenuLevel);
+	}
+
+	/* -----------------------------------------------------------------------------------------------------------
+	 * MY STUFF HERE
+	 * -----------------------------------------------------------------------------------------------------------
+	 */
+	void ProcessMovementMenuLevel(int nCurrentMenuLevel) {
+
+		if(nCurrentMenuLevel == 0) {
+			
+			// DEBUG
+			txCurrentCursor = null;
+			stMenuMessage = null;
+		}
+		// First menu level: the player pressed 'm' to start movement;
+		if(nCurrentMenuLevel == 1) {
+			
+			// DEBUG
+			txCurrentCursor = txCursorSelectPosition;
+			stMenuMessage = "[MOVE] Select target position";
+		}
+		// The player confirmed the target position. Now he must select the ship orientation
+		if(nCurrentMenuLevel == 2) {
+			
+			// DEBUG
+			txCurrentCursor = txCursorSelectPosition;
+			stMenuMessage = "[MOVE] Select ship orientation";
+		}
+	}
+
+	/// <summary>
+	///
+	/// </summary>
+	void OnGUI() {
+
+		if(txCurrentCursor != null) {
+
+			vGUICursorPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f);
+			GUI.DrawTexture(new Rect(vGUICursorPosition.x - 16, Screen.height - vGUICursorPosition.y - 16, 32, 32), txCurrentCursor);
+		}
+	
+		// Shows a text message on the screen with the current movement status
+		if(stMenuMessage != null) {
+
+			GUI.Label(new Rect(100, Screen.height - 200, 200, 100), stMenuMessage);
+		}
+	}
+}
---
+++ Assets/Scripts/InputGenericMouseCursorPosition.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Class description here
+/// </summary>
+public class InputGenericMouseCursorPosition : MonoBehaviour {
+	
+	// PUBLIC
+	public Texture2D		txCursor;					//< Texture of the cursor on the screen
+	Vector3							vCursorNow;				//< Current mouse position
+	Vector3							vCursorDirection;	//< Direction to the cursor in world coordinates
+
+	public bool					bnUseGamepad = false;	//< Using mouse or gamepad?
+	/* -----------------------------------------------------------------------------------------------------------
+	 * UNITY'S STUFF HERE
+	 * -----------------------------------------------------------------------------------------------------------
+	 */
+	/// <summary>
+	/// Will execute when loaded
+	/// </summary>
+	void Awake() {
+
+	}
+
+	/// <summary>
+	/// Start will execute when an object with it is instantiated
+	/// </summary>
+	void Start () {
+	
+	}
+	
+	/// <summary>
+	/// Update is called once per frame
+	/// </summary>
+	void Update () {
+	
+		if(!bnUseGamepad) {
+			// Using the mouse instead
+			vCursorNow = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+			
+			// Position of the mouse cursor in the world
+			vCursorDirection =  vCursorNow - transform.position;
+		}
+	}
+
+	/* -----------------------------------------------------------------------------------------------------------
+	 * MY STUFF HERE
+	 * -----------------------------------------------------------------------------------------------------------
+	 */
+
+	/// <summary>
+	/// Returns the current mouse position in world coordinates
+	/// </summary>
+	public Vector3 GetCurrentCursorPosition() {
+
+		return vCursorNow;
+	}
+
+	/// <summary>
+	/// Returns the vector from this position to the mouse cursor
+	/// </summary>
+	public Vector3 GetCurrentDirectionToCursorPosition() {
+
+		return vCursorDirection;
+	}
+}
