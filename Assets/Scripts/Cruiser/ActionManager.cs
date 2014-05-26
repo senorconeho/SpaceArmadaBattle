9d17388919caeaaae3b937cc765a354a688d91c3
jinay <riberodrigo@gmail.com>
2014-05-26 02:08:14 -0300

    Controle do Cruzador
    Iniciando NOVAMENTE e no repositorio certo o controle de partes da nave
    e incluindo um tiro tosco de canhão.

---
+++ Assets/Scripts/Cruiser/ActionManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionManager : MonoBehaviour {
+
+	public enum Action{
+		CANNON_SHOT,
+		SHIELD_DIRECTION,
+		REPAIR
+	};
+
+	public enum Direction{
+		LEFT,
+		RIGHT
+	};
+
+	public Action act = Action.CANNON_SHOT;
+	public Transform shootPoint;
+	public GameObject shootType;
+	public Direction shootDirection;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void DoAction(){
+
+		if (act == Action.CANNON_SHOT){
+			CannonShot();
+		}
+	}
+
+	void CannonShot(){
+
+		Instantiate(shootType,shootPoint.transform.position,shootPoint.transform.rotation);
+
+	}
+}
---
+++ Assets/Scripts/Cruiser/CruiserManagement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CruiserManagement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		ControlObject();
+	
+	}
+
+	void ControlObject(){
+		
+		RaycastHit hit = new RaycastHit();
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		
+		if(Physics.Raycast(ray, out hit, 20)){
+			
+			Debug.Log (hit.transform.name);
+			if(hit.transform.GetComponent<ActionManager>()){
+				Click(hit);
+			}
+
+		}
+	}
+
+	void Click(RaycastHit h){
+		if(Input.GetMouseButtonDown(0))
+			h.transform.GetComponent<ActionManager>().DoAction();
+	}
+}
