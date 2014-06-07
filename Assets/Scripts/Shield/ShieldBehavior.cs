1f47fbc1f0e9f71b070f24734ec4fd92fc666a6d
jinay <riberodrigo@gmail.com>
2014-06-07 18:16:21 -0300

    escudos
    iniciando

---
+++ Assets/Scripts/Shield/ShieldBehavior.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShieldBehavior : MonoBehaviour {
+
+	public float alpha;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	
+	}
+}
